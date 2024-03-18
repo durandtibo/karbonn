ef57eecd045f60ba312f3aae969bff82c70b2bc5
Thibaut Durand <durand.tibo@gmail.com>
2024-03-17 17:18:10 -0700

    Add `Clamp` module

---
+++ tests/unit/test_clamp.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+import pytest
+import torch
+from coola.utils.tensor import get_available_devices
+
+from karbonn import Clamp
+
+SIZES = (1, 2)
+
+
+###########################
+#     Tests for Clamp     #
+###########################
+
+
+def test_clamp_str() -> None:
+    assert str(Clamp()).startswith("Clamp(")
+
+
+@pytest.mark.parametrize("device", get_available_devices())
+@pytest.mark.parametrize("dtype", [torch.float, torch.long])
+def test_clamp_forward(device: str, dtype: torch.dtype) -> None:
+    device = torch.device(device)
+    module = Clamp().to(device=device)
+    assert module(torch.arange(-3, 4, dtype=dtype, device=device)).equal(
+        torch.tensor([-1.0, -1.0, -1.0, 0.0, 1.0, 1.0, 1.0], dtype=torch.float, device=device)
+    )
+
+
+@pytest.mark.parametrize("device", get_available_devices())
+def test_clamp_forward_min_0(device: str) -> None:
+    device = torch.device(device)
+    module = Clamp(min=0.0).to(device=device)
+    assert module(torch.arange(-3, 4, device=device)).equal(
+        torch.tensor([0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0], dtype=torch.float, device=device)
+    )
+
+
+@pytest.mark.parametrize("device", get_available_devices())
+def test_clamp_forward_max_2(device: str) -> None:
+    device = torch.device(device)
+    module = Clamp(max=2).to(device=device)
+    assert module(torch.arange(-3, 4, device=device)).equal(
+        torch.tensor([-1.0, -1.0, -1.0, 0.0, 1.0, 2.0, 2.0], dtype=torch.float, device=device)
+    )
