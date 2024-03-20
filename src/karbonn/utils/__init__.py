dfe684d366cf1231d982cf893bad2dd37e1c20d4
Thibaut Durand <durand.tibo@gmail.com>
2024-03-19 22:17:58 -0700

    Add factory functions (#9)

---
+++ src/karbonn/utils/__init__.py
@@ -0,0 +1,7 @@
+r"""Contain utility functions."""
+
+from __future__ import annotations
+
+__all__ = ["is_module_config", "setup_module"]
+
+from karbonn.utils.factory import is_module_config, setup_module
