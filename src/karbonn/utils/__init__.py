40af9ca27da08f0058e023679fd66ec94ff3fefb
Thibaut Durand <durand.tibo@gmail.com>
2024-03-22 20:22:53 -0700

    Add `module_mode` context manager (#14)

--- src/karbonn/utils/__init__.py
+++ src/karbonn/utils/__init__.py
@@ -2,6 +2,11 @@ r"""Contain utility functions."""
 
 from __future__ import annotations
 
-__all__ = ["is_module_config", "setup_module"]
-
+__all__ = [
+    "is_module_config",
+    "module_mode",
+    "setup_module",
+    "top_module_mode",
+]
 from karbonn.utils.factory import is_module_config, setup_module
+from karbonn.utils.mode import module_mode, top_module_mode
