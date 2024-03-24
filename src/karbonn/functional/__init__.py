f090af0bc281cc208d4a33bcf3c407178a88e0f0
Thibaut Durand <durand.tibo@gmail.com>
2024-03-24 11:02:19 -0700

    Add reduction functions (#29)

--- src/karbonn/functional/__init__.py
+++ src/karbonn/functional/__init__.py
@@ -2,6 +2,12 @@ r"""Contain functional implementation of some modules."""
 
 from __future__ import annotations
 
-__all__ = ["safe_exp", "safe_log"]
+__all__ = [
+    "safe_exp",
+    "safe_log",
+    "reduce_loss",
+    "check_loss_reduction_strategy",
+]
 
 from karbonn.functional.activations import safe_exp, safe_log
+from karbonn.functional.reduction import check_loss_reduction_strategy, reduce_loss
