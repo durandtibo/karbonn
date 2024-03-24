1771d6d230465fae950daddcc49954a73c8505bb
Thibaut Durand <durand.tibo@gmail.com>
2024-03-23 19:36:08 -0700

    Add activation modules

--- src/karbonn/__init__.py
+++ src/karbonn/__init__.py
@@ -2,7 +2,46 @@ r"""Root package."""
 
 from __future__ import annotations
 
-__all__ = ["Clamp", "is_module_config", "setup_module"]
+__all__ = [
+    "Clamp",
+    "ReLUn",
+    "Snake",
+    "SquaredReLU",
+    "freeze_module",
+    "get_module_device",
+    "get_module_devices",
+    "has_learnable_parameters",
+    "has_parameters",
+    "is_loss_decreasing",
+    "is_loss_decreasing_with_adam",
+    "is_loss_decreasing_with_sgd",
+    "is_module_config",
+    "is_module_on_device",
+    "module_mode",
+    "num_learnable_parameters",
+    "num_parameters",
+    "setup_module",
+    "top_module_mode",
+    "unfreeze_module",
+]
 
+from karbonn.activations import ReLUn, Snake, SquaredReLU
 from karbonn.clamp import Clamp
-from karbonn.utils import is_module_config, setup_module
+from karbonn.utils import (
+    freeze_module,
+    get_module_device,
+    get_module_devices,
+    has_learnable_parameters,
+    has_parameters,
+    is_loss_decreasing,
+    is_loss_decreasing_with_adam,
+    is_loss_decreasing_with_sgd,
+    is_module_config,
+    is_module_on_device,
+    module_mode,
+    num_learnable_parameters,
+    num_parameters,
+    setup_module,
+    top_module_mode,
+    unfreeze_module,
+)
---
+++ tests/unit/activations/test_relu.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+import pytest
+import torch
+from coola.utils.tensor import get_available_devices
+
+from karbonn import ReLUn, SquaredReLU
+
+SIZES = ((1, 1), (2, 3), (2, 3, 4), (2, 3, 4, 5))
+
+###########################
+#     Tests for ReLUn     #
+###########################
+
+
+def test_relun_str() -> None:
+    assert str(ReLUn()).startswith("ReLUn(")
+
+
+@pytest.mark.parametrize("device", get_available_devices())
+@pytest.mark.parametrize("dtype", [torch.float, torch.long])
+def test_relun_forward(device: str, dtype: torch.dtype) -> None:
+    device = torch.device(device)
+    module = ReLUn().to(device=device)
+    assert module(torch.arange(-1, 4, dtype=dtype, device=device)).equal(
+        torch.tensor([0.0, 0.0, 1.0, 1.0, 1.0], dtype=torch.float, device=device)
+    )
+
+
+@pytest.mark.parametrize("device", get_available_devices())
+def test_relun_forward_max_value_2(device: str) -> None:
+    device = torch.device(device)
+    module = ReLUn(max=2).to(device=device)
+    assert module(torch.arange(-1, 4, dtype=torch.float, device=device)).equal(
+        torch.tensor([0.0, 0.0, 1.0, 2.0, 2.0], dtype=torch.float, device=device)
+    )
+
+
+@pytest.mark.parametrize("size", SIZES)
+def test_relun_forward_size(size: tuple[int, ...]) -> None:
+    module = ReLUn()
+    out = module(torch.randn(*size))
+    assert out.shape == size
+    assert out.dtype == torch.float
+
+
+#################################
+#     Tests for SquaredReLU     #
+#################################
+
+
+@pytest.mark.parametrize("device", get_available_devices())
+@pytest.mark.parametrize("dtype", [torch.float, torch.long])
+def test_squared_relu_forward(device: str, dtype: torch.dtype) -> None:
+    device = torch.device(device)
+    module = SquaredReLU().to(device=device)
+    assert module(torch.arange(-1, 4, dtype=dtype, device=device)).equal(
+        torch.tensor([0.0, 0.0, 1.0, 4.0, 9.0], dtype=torch.float, device=device)
+    )
+
+
+@pytest.mark.parametrize("size", SIZES)
+def test_squared_relu_forward_size(size: tuple[int, ...]) -> None:
+    module = SquaredReLU()
+    out = module(torch.randn(*size))
+    assert out.shape == size
+    assert out.dtype == torch.float
