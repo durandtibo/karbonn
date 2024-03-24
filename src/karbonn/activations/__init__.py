2689c6460e37fe70baa0932554698f032d94eec7
Thibaut Durand <durand.tibo@gmail.com>
2024-03-23 19:53:37 -0700

    Add activation modules (#19)

---
+++ src/karbonn/activations/__init__.py
@@ -0,0 +1,8 @@
+r"""Contain activation modules."""
+
+from __future__ import annotations
+
+__all__ = ["ReLUn", "Snake", "SquaredReLU"]
+
+from karbonn.activations.relu import ReLUn, SquaredReLU
+from karbonn.activations.snake import Snake
