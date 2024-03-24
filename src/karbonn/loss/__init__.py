9dcc9fd752994027251a0ea0d7e49ef00603f5ad
Thibaut Durand <durand.tibo@gmail.com>
2024-03-24 10:34:21 -0700

    Add focal loss

---
+++ src/karbonn/loss/__init__.py
@@ -0,0 +1,11 @@
+r"""Contain loss functions."""
+
+from __future__ import annotations
+
+__all__ = ["BaseBinaryFocalLoss", "BinaryFocalLoss", "BinaryFocalLossWithLogits"]
+
+from karbonn.loss.focal import (
+    BaseBinaryFocalLoss,
+    BinaryFocalLoss,
+    BinaryFocalLossWithLogits,
+)
