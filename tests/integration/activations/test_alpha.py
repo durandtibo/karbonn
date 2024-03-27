2afcff1ca370f05a3d4c311791a6db018e047d85
Thibaut Durand <durand.tibo@gmail.com>
2024-03-26 21:37:09 -0700

    Improve activation layers

--- tests/integration/activations/test_alpha.py
+++ tests/integration/activations/test_alpha.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import pytest
 import torch
 from torch import nn
 
@@ -11,66 +12,25 @@ from karbonn.utils import is_loss_decreasing_with_adam
 ############################
 
 
-def test_exp_sin_is_loss_decreasing() -> None:
+@pytest.mark.parametrize(
+    "activation",
+    [
+        ExpSin(),
+        ExpSin(num_parameters=6),
+        Gaussian(),
+        Gaussian(num_parameters=6),
+        Laplacian(),
+        Laplacian(num_parameters=6),
+        MultiQuadratic(),
+        MultiQuadratic(num_parameters=6),
+        Quadratic(),
+        Quadratic(num_parameters=6),
+    ],
+)
+def test_activation_is_loss_decreasing(activation: nn.Module) -> None:
     assert is_loss_decreasing_with_adam(
-        module=ExpSin(num_parameters=4),
+        module=nn.Sequential(nn.Linear(6, 6), activation),
         criterion=nn.MSELoss(),
-        feature=torch.randn(8, 4),
-        target=torch.randn(8, 4),
-    )
-
-
-##############################
-#     Tests for Gaussian     #
-##############################
-
-
-def test_gaussian_is_loss_decreasing() -> None:
-    assert is_loss_decreasing_with_adam(
-        module=Gaussian(num_parameters=4),
-        criterion=nn.MSELoss(),
-        feature=torch.randn(8, 4),
-        target=torch.randn(8, 4),
-    )
-
-
-###############################
-#     Tests for Laplacian     #
-###############################
-
-
-def test_laplacian_is_loss_decreasing() -> None:
-    assert is_loss_decreasing_with_adam(
-        module=Laplacian(num_parameters=4),
-        criterion=nn.MSELoss(),
-        feature=torch.randn(8, 4),
-        target=torch.randn(8, 4),
-    )
-
-
-####################################
-#     Tests for MultiQuadratic     #
-####################################
-
-
-def test_multi_quadratic_is_loss_decreasing() -> None:
-    assert is_loss_decreasing_with_adam(
-        module=MultiQuadratic(num_parameters=4),
-        criterion=nn.MSELoss(),
-        feature=torch.randn(8, 4),
-        target=torch.randn(8, 4),
-    )
-
-
-###############################
-#     Tests for Quadratic     #
-###############################
-
-
-def test_quadratic_is_loss_decreasing() -> None:
-    assert is_loss_decreasing_with_adam(
-        module=Quadratic(num_parameters=4),
-        criterion=nn.MSELoss(),
-        feature=torch.randn(8, 4),
-        target=torch.randn(8, 4),
+        feature=torch.randn(8, 6),
+        target=torch.randn(8, 6),
     )
