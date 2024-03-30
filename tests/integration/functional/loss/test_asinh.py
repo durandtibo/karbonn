e1aada6acc02d6bf553f5f1ae28c94aac9e7d04d
Thibaut Durand <durand.tibo@gmail.com>
2024-03-29 23:02:07 -0700

    Add `asinh_smooth_l1_loss`

--- tests/integration/functional/loss/test_asinh.py
+++ tests/integration/functional/loss/test_asinh.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import torch
 from torch import nn
 
-from karbonn.functional import asinh_mse_loss
+from karbonn.functional import asinh_mse_loss, asinh_smooth_l1_loss
 from karbonn.utils import is_loss_decreasing_with_sgd
 
 ####################################
@@ -15,6 +15,20 @@ def test_asinh_mse_loss_is_loss_decreasing() -> None:
     assert is_loss_decreasing_with_sgd(
         module=nn.Linear(4, 2),
         criterion=asinh_mse_loss,
+        feature=torch.rand(4, 4),
+        target=torch.rand(4, 2),
+    )
+
+
+##########################################
+#     Tests for asinh_smooth_l1_loss     #
+##########################################
+
+
+def test_asinh_smooth_l1_loss_is_loss_decreasing() -> None:
+    assert is_loss_decreasing_with_sgd(
+        module=nn.Linear(4, 2),
+        criterion=asinh_smooth_l1_loss,
         feature=torch.rand(4, 4),
         target=torch.rand(4, 2),
     )
