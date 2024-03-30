248cc4d2e1ba2128f2eff70acdf2d79fb091c0dd
Thibaut Durand <durand.tibo@gmail.com>
2024-03-29 19:10:29 -0700

    Minor improvements (#66)

--- src/karbonn/utils/size/__init__.py
+++ src/karbonn/utils/size/__init__.py
@@ -29,12 +29,12 @@ __all__ = [
 from karbonn.utils.size.attention import MultiheadAttentionSizeFinder
 from karbonn.utils.size.auto import AutoSizeFinder, register_size_finders
 from karbonn.utils.size.base import BaseSizeFinder
-from karbonn.utils.size.batchnorm import BatchNormSizeFinder
 from karbonn.utils.size.conv import ConvolutionSizeFinder
 from karbonn.utils.size.embedding import EmbeddingSizeFinder
 from karbonn.utils.size.functional import find_in_features, find_out_features
 from karbonn.utils.size.linear import BilinearSizeFinder, LinearSizeFinder
 from karbonn.utils.size.list import ModuleListSizeFinder
+from karbonn.utils.size.norm import BatchNormSizeFinder
 from karbonn.utils.size.recurrent import RecurrentSizeFinder
 from karbonn.utils.size.sequential import SequentialSizeFinder
 from karbonn.utils.size.transformer import (
