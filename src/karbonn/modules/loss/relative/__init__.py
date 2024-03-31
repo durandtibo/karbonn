f5e9e5a0fe9007d73ff34a330bfcd14f71264459
Thibaut Durand <durand.tibo@gmail.com>
2024-03-30 20:54:31 -0700

    Refactor modules

--- src/karbonn/modules/loss/relative/__init__.py
+++ src/karbonn/modules/loss/relative/__init__.py
@@ -1,28 +1,25 @@
-r"""Contain loss functions."""
+r"""Contain relative loss functions."""
 
 from __future__ import annotations
 
 __all__ = [
     "ArithmeticalMeanIndicator",
     "BaseRelativeIndicator",
-    "BinaryFocalLoss",
     "ClassicalRelativeIndicator",
-    "GeneralRobustRegressionLoss",
     "RelativeLoss",
     "RelativeMSELoss",
     "RelativeSmoothL1Loss",
     "ReversedRelativeIndicator",
-    "binary_focal_loss",
 ]
 
-from karbonn.modules.loss.focal import BinaryFocalLoss, binary_focal_loss
-from karbonn.modules.loss.general_robust import GeneralRobustRegressionLoss
-from karbonn.modules.loss.relative import (
+from karbonn.modules.loss.relative.indicators import (
     ArithmeticalMeanIndicator,
     BaseRelativeIndicator,
     ClassicalRelativeIndicator,
+    ReversedRelativeIndicator,
+)
+from karbonn.modules.loss.relative.relative import (
     RelativeLoss,
     RelativeMSELoss,
     RelativeSmoothL1Loss,
-    ReversedRelativeIndicator,
 )
