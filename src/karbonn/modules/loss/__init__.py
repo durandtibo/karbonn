846c19286720dd5cf5b102e67d07e5c56dd46573
Thibaut Durand <durand.tibo@gmail.com>
2024-03-30 20:44:43 -0700

    Add relative indicator functions (#75)

--- src/karbonn/modules/loss/__init__.py
+++ src/karbonn/modules/loss/__init__.py
@@ -3,16 +3,26 @@ r"""Contain loss functions."""
 from __future__ import annotations
 
 __all__ = [
+    "ArithmeticalMeanIndicator",
+    "BaseRelativeIndicator",
     "BinaryFocalLoss",
+    "ClassicalRelativeIndicator",
     "GeneralRobustRegressionLoss",
     "RelativeLoss",
     "RelativeMSELoss",
     "RelativeSmoothL1Loss",
+    "ReversedRelativeIndicator",
     "binary_focal_loss",
 ]
 
 from karbonn.modules.loss.focal import BinaryFocalLoss, binary_focal_loss
 from karbonn.modules.loss.general_robust import GeneralRobustRegressionLoss
+from karbonn.modules.loss.indicators import (
+    ArithmeticalMeanIndicator,
+    BaseRelativeIndicator,
+    ClassicalRelativeIndicator,
+    ReversedRelativeIndicator,
+)
 from karbonn.modules.loss.relative import (
     RelativeLoss,
     RelativeMSELoss,
---
+++ tests/unit/modules/loss/test_indicators.py
@@ -0,0 +1,77 @@
+from __future__ import annotations
+
+import pytest
+import torch
+from coola import objects_are_equal
+from coola.utils.tensor import get_available_devices
+
+from karbonn.modules.loss import (
+    ArithmeticalMeanIndicator,
+    ClassicalRelativeIndicator,
+    ReversedRelativeIndicator,
+)
+
+###############################################
+#     Tests for ArithmeticalMeanIndicator     #
+###############################################
+
+
+def test_arithmetical_mean_indicator_str() -> None:
+    assert str(ArithmeticalMeanIndicator()).startswith("ArithmeticalMeanIndicator(")
+
+
+@pytest.mark.parametrize("device", get_available_devices())
+def test_arithmetical_mean_indicator_forward(device: str) -> None:
+    device = torch.device(device)
+    indicator = ArithmeticalMeanIndicator()
+    assert objects_are_equal(
+        indicator(
+            torch.tensor([[0.0, 1.0, -1.0], [3.0, 1.0, -1.0]], device=device),
+            torch.tensor([[-2.0, 1.0, 0.0], [-3.0, 5.0, -1.0]], device=device),
+        ),
+        torch.tensor([[1.0, 1.0, 0.5], [3.0, 3.0, 1.0]], device=device),
+    )
+
+
+################################################
+#     Tests for ClassicalRelativeIndicator     #
+################################################
+
+
+def test_classical_relative_indicator_str() -> None:
+    assert str(ClassicalRelativeIndicator()).startswith("ClassicalRelativeIndicator(")
+
+
+@pytest.mark.parametrize("device", get_available_devices())
+def test_classical_relative_indicator_forward(device: str) -> None:
+    device = torch.device(device)
+    indicator = ClassicalRelativeIndicator()
+    assert objects_are_equal(
+        indicator(
+            torch.ones(2, 3, device=device),
+            torch.tensor([[-2.0, 1.0, 0.0], [-3.0, 5.0, -1.0]], device=device),
+        ),
+        torch.tensor([[2.0, 1.0, 0.0], [3.0, 5.0, 1.0]], device=device),
+    )
+
+
+###############################################
+#     Tests for ReversedRelativeIndicator     #
+###############################################
+
+
+def test_reversed_relative_indicator_str() -> None:
+    assert str(ReversedRelativeIndicator()).startswith("ReversedRelativeIndicator(")
+
+
+@pytest.mark.parametrize("device", get_available_devices())
+def test_reversed_relative_indicator_forward(device: str) -> None:
+    device = torch.device(device)
+    indicator = ReversedRelativeIndicator()
+    assert objects_are_equal(
+        indicator(
+            torch.tensor([[-2.0, 1.0, 0.0], [-3.0, 5.0, -1.0]], device=device),
+            torch.ones(2, 3, device=device),
+        ),
+        torch.tensor([[2.0, 1.0, 0.0], [3.0, 5.0, 1.0]], device=device),
+    )
