ac095fbe923684bf2ae848ae9cfd57ed57fbef4a
Thibaut Durand <durand.tibo@gmail.com>
2024-03-31 22:09:59 -0700

    Add `ParameterSummary`

---
+++ src/karbonn/utils/summary/__init__.py
@@ -0,0 +1,18 @@
+r"""Contain utility functions to summarize modules and their
+parameters."""
+
+from __future__ import annotations
+
+__all__ = [
+    "NO_PARAMETER",
+    "PARAMETER_NOT_INITIALIZED",
+    "ParameterSummary",
+    "get_parameter_summaries",
+]
+
+from karbonn.utils.summary.parameter import (
+    NO_PARAMETER,
+    PARAMETER_NOT_INITIALIZED,
+    ParameterSummary,
+    get_parameter_summaries,
+)
