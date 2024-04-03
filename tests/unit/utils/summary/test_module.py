61ddf3b7910e6bdfcdf3f159f6876fc482b93b9a
Thibaut Durand <durand.tibo@gmail.com>
2024-04-02 22:33:14 -0700

    Add `parse_batch_dtype` (#99)

--- tests/unit/utils/summary/test_module.py
+++ tests/unit/utils/summary/test_module.py
@@ -6,12 +6,83 @@ from typing import TYPE_CHECKING
 import pytest
 import torch
 
-from karbonn.utils.summary.module import parse_batch_shape
+from karbonn.utils.summary.module import parse_batch_dtype, parse_batch_shape
 
 if TYPE_CHECKING:
     from collections.abc import Mapping, Sequence
 
 SIZES = [1, 2, 3]
+
+
+#######################################
+#     Tests for parse_batch_dtype     #
+#######################################
+
+
+@pytest.mark.parametrize(
+    "dtype",
+    [
+        torch.bool,
+        torch.uint8,
+        torch.int,
+        torch.int16,
+        torch.int32,
+        torch.int64,
+        torch.long,
+        torch.float,
+        torch.float16,
+        torch.float32,
+        torch.float64,
+        torch.cdouble,
+        torch.complex64,
+    ],
+)
+def test_parse_batch_dtype_tensor(dtype: torch.dtype) -> None:
+    assert parse_batch_dtype(torch.ones(2, 3, dtype=dtype)) == dtype
+
+
+@pytest.mark.parametrize(
+    "batch",
+    [
+        (torch.ones(2, 3), torch.ones(2, dtype=torch.long)),
+        [torch.ones(2, 3), torch.ones(2, dtype=torch.long)],
+    ],
+)
+def test_parse_batch_dtype_sequence(batch: Sequence) -> None:
+    assert parse_batch_dtype(batch) == (torch.float32, torch.int64)
+
+
+def test_parse_batch_dtype_sequence_empty() -> None:
+    assert parse_batch_dtype([]) == ()
+
+
+@pytest.mark.parametrize(
+    "batch",
+    [
+        {"feature1": torch.ones(2, 3), "feature2": torch.ones(2, dtype=torch.long)},
+        OrderedDict({"feature1": torch.ones(2, 3), "feature2": torch.ones(2, dtype=torch.long)}),
+    ],
+)
+def test_parse_batch_dtype_dict(batch: Mapping[str, torch.Tensor]) -> None:
+    assert parse_batch_dtype(batch) == {"feature1": torch.float32, "feature2": torch.long}
+
+
+def test_parse_batch_dtype_dict_empty() -> None:
+    assert parse_batch_dtype({}) == {}
+
+
+def test_parse_batch_dtype_invalid() -> None:
+    assert parse_batch_dtype(set()) is None
+
+
+def test_parse_batch_dtype_nested() -> None:
+    assert parse_batch_dtype(
+        (torch.ones(2, 3), (torch.ones(2, dtype=torch.long), torch.ones(2, 3)))
+    ) == (
+        torch.float32,
+        (torch.int64, torch.float32),
+    )
+
 
 #######################################
 #     Tests for parse_batch_shape     #
@@ -65,3 +136,12 @@ def test_parse_batch_shape_dict_empty() -> None:
 
 def test_parse_batch_shape_invalid() -> None:
     assert parse_batch_shape(set()) is None
+
+
+def test_parse_batch_shape_nested() -> None:
+    assert parse_batch_shape(
+        (torch.ones(2, 3), (torch.ones(2, dtype=torch.long), torch.ones(2, 3)))
+    ) == (
+        torch.Size([2, 3]),
+        (torch.Size([2]), torch.Size([2, 3])),
+    )
