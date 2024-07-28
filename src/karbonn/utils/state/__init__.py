8b3c31f785ac6515abc301fee6f39fd7e223de34
Thibaut Durand <durand.tibo@gmail.com>
2024-07-27 17:05:04 -0700

    Add `MovingAverageState`

--- src/karbonn/utils/state/__init__.py
+++ src/karbonn/utils/state/__init__.py
@@ -2,8 +2,15 @@ r"""Contain state implementations for metrics."""
 
 from __future__ import annotations
 
-__all__ = ["AverageState", "EmptyStateError", "ScalarState"]
+__all__ = [
+    "AverageState",
+    "EmptyStateError",
+    "ExponentialMovingAverageState",
+    "MovingAverageState",
+    "ScalarState",
+]
 
 from karbonn.utils.state.average import AverageState
 from karbonn.utils.state.exception import EmptyStateError
+from karbonn.utils.state.moving import ExponentialMovingAverageState, MovingAverageState
 from karbonn.utils.state.scalar import ScalarState
