efff1fb3798b101ed34f837b751371dbc0d24ea0
Thibaut Durand <durand.tibo@gmail.com>
2024-07-30 23:59:38 -0700

    Add `tensor.quantile` (#213)

--- src/karbonn/utils/tensor/__init__.py
+++ src/karbonn/utils/tensor/__init__.py
@@ -2,6 +2,7 @@ r"""Contain tensor utility functions."""
 
 from __future__ import annotations
 
-__all__ = ["to_tensor"]
+__all__ = ["quantile", "to_tensor"]
 
 from karbonn.utils.tensor.conversion import to_tensor
+from karbonn.utils.tensor.mathops import quantile
