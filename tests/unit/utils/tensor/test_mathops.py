6cce24de0c817ecc0057ff7679a56fb47bb439a7
Thibaut Durand <durand.tibo@gmail.com>
2024-07-30 23:47:54 -0700

    Add `tensor.quantile`

--- tests/unit/utils/tensor/test_mathops.py
+++ tests/unit/utils/tensor/test_mathops.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import pytest
 import torch
+from coola.utils.tensor import get_available_devices
 
 from karbonn.utils.tensor import quantile
 
@@ -14,9 +15,24 @@ DTYPES = (torch.float, torch.long)
 
 
 @pytest.mark.parametrize("dtype", DTYPES)
-def test_quantile_dtype(dtype: torch.dtype) -> None:
+def test_quantile_input_dtype(dtype: torch.dtype) -> None:
     assert quantile(torch.arange(11).to(dtype=dtype), q=torch.tensor([0.1])).equal(
         torch.tensor([1], dtype=torch.float),
+    )
+
+
+@pytest.mark.parametrize("dtype", DTYPES)
+def test_quantile_output_dtype(dtype: torch.dtype) -> None:
+    assert quantile(torch.arange(11), q=torch.tensor([0.1])).equal(
+        torch.tensor([1], dtype=dtype),
+    )
+
+
+@pytest.mark.parametrize("device", get_available_devices())
+def test_quantile_device(device: str) -> None:
+    device = torch.device(device)
+    assert quantile(torch.arange(11, device=device), q=torch.tensor([0.1])).equal(
+        torch.tensor([1], device=device),
     )
 
 
