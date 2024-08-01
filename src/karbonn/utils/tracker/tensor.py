e2b3d576a62aa4d70f0d1685c671cb892772d0e4
Thibaut Durand <durand.tibo@gmail.com>
2024-08-01 15:15:50 -0700

    Add `TensorTracker` (#211)

--- src/karbonn/utils/tracker/tensor.py
+++ src/karbonn/utils/tracker/tensor.py
@@ -2,13 +2,15 @@ r"""Contain trackers for tensors."""
 
 from __future__ import annotations
 
-__all__ = ["MeanTensorTracker", "ExtremaTensorTracker"]
+__all__ = ["MeanTensorTracker", "ExtremaTensorTracker", "TensorTracker"]
 
 from typing import TYPE_CHECKING, Any
 
 import torch
+from coola import objects_are_equal
 
 from karbonn.distributed.ddp import MAX, MIN, SUM, sync_reduce
+from karbonn.utils.tensor import FlattenBuffer, quantile
 from karbonn.utils.tracker.exception import EmptyTrackerError
 
 if TYPE_CHECKING:
@@ -478,7 +480,7 @@ class ExtremaTensorTracker:
             The max value.
 
         Raises:
-            EmptyTrackerError:if the tracker is empty.
+            EmptyTrackerError: if the tracker is empty.
 
         Example usage:
 
@@ -726,3 +728,502 @@ class ExtremaTensorTracker:
         ```
         """
         return {"count": self._count, "max_value": self._max_value, "min_value": self._min_value}
+
+
+class TensorTracker:
+    r"""Implement a tracker to compute some stats on ``torch.Tensor``s.
+
+    This tracker has a linear space complexity as its store all the
+    values. You cannot use this tracker if you want to track a large
+    number of values that cannot be store in memory.
+
+    Args:
+        values: The initial values. The tensor is flattened if
+            necessary. ``None`` means no initial values.
+
+    Example usage:
+
+    ```pycon
+
+    >>> import torch
+    >>> from karbonn.utils.tracker import TensorTracker
+    >>> tracker = TensorTracker()
+    >>> tracker.update(torch.arange(6))
+    >>> tracker.update(torch.tensor([4.0, 1.0]))
+    >>> tracker.count
+    8
+    >>> tracker.mean()
+    2.5
+    >>> tracker.max()
+    5.0
+    >>> tracker.min()
+    0.0
+    >>> tracker.sum()
+    20.0
+    >>> tracker.median()
+    2.0
+    >>> tracker.quantile(torch.tensor([0.1, 0.5]))
+    tensor([0.7000, 2.5000])
+    >>> tracker.std()
+    1.772...
+
+    ```
+    """
+
+    def __init__(self, values: Tensor | None = None) -> None:
+        self._values = FlattenBuffer(values)
+        self._count = self._values.numel()
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__qualname__}(count={self._count:,})"
+
+    @property
+    def count(self) -> int:
+        r"""The number of predictions in the tracker."""
+        return self._count
+
+    def reset(self) -> None:
+        r"""Reset the tracker.
+
+        Example usage:
+
+        ```pycon
+
+        >>> import torch
+        >>> from karbonn.utils.tracker import TensorTracker
+        >>> tracker = TensorTracker(torch.arange(6))
+        >>> tracker.reset()
+        >>> tracker.count
+        0
+
+        ```
+        """
+        self._count = 0
+        self._values.clear()
+
+    def update(self, tensor: Tensor) -> None:
+        r"""Update the tracker given a new tensor.
+
+        Args:
+            tensor: The new tensor to add to the tracker.
+
+        Example usage:
+
+        ```pycon
+
+        >>> import torch
+        >>> from karbonn.utils.tracker import TensorTracker
+        >>> tracker = TensorTracker()
+        >>> tracker.update(torch.arange(6))
+        >>> tracker.count
+        6
+        >>> tracker.sum()
+        15
+
+        ```
+        """
+        self._values.update(tensor.detach())
+        self._count += tensor.numel()
+
+    def average(self) -> float:
+        r"""Compute the average value.
+
+        Returns:
+            The average value.
+
+        Raises:
+            EmptyTrackerError: if the tracker is empty.
+
+        Example usage:
+
+        ```pycon
+
+        >>> import torch
+        >>> from karbonn.utils.tracker import TensorTracker
+        >>> tracker = TensorTracker()
+        >>> tracker.update(torch.arange(6))
+        >>> tracker.average()
+        2.5
+
+        ```
+        """
+        return self.mean()
+
+    def max(self) -> int | float:
+        r"""Get the max value.
+
+        Returns:
+            int or The max value.
+
+        Raises:
+            EmptyTrackerError: if the tracker is empty.
+
+        Example usage:
+
+        ```pycon
+
+        >>> import torch
+        >>> from karbonn.utils.tracker import TensorTracker
+        >>> tracker = TensorTracker()
+        >>> tracker.update(torch.arange(6))
+        >>> tracker.max()
+        5
+
+        ```
+        """
+        if not self._count:
+            msg = "The tracker is empty"
+            raise EmptyTrackerError(msg)
+        return self._values.values().max().item()
+
+    def mean(self) -> float:
+        r"""Get the mean value.
+
+        Returns:
+            The mean value.
+
+        Raises:
+            EmptyTrackerError: if the tracker is empty.
+
+        Example usage:
+
+        ```pycon
+
+        >>> import torch
+        >>> from karbonn.utils.tracker import TensorTracker
+        >>> tracker = TensorTracker()
+        >>> tracker.update(torch.arange(6))
+        >>> tracker.average()
+        2.5
+
+        ```
+        """
+        if not self._count:
+            msg = "The tracker is empty"
+            raise EmptyTrackerError(msg)
+        return self._values.values().float().mean().item()
+
+    def median(self) -> float:
+        r"""Get the median value.
+
+        Returns:
+            The median value.
+
+        Raises:
+            EmptyTrackerError: if the tracker is empty.
+
+        Example usage:
+
+        ```pycon
+
+        >>> import torch
+        >>> from karbonn.utils.tracker import TensorTracker
+        >>> tracker = TensorTracker()
+        >>> tracker.update(torch.arange(5))
+        >>> tracker.median()
+        2
+
+        ```
+        """
+        if not self._count:
+            msg = "The tracker is empty"
+            raise EmptyTrackerError(msg)
+        return self._values.values().median().item()
+
+    def min(self) -> int | float:
+        r"""Get the min value.
+
+        Returns:
+            The min value.
+
+        Raises:
+            EmptyTrackerError: if the tracker is empty.
+
+        Example usage:
+
+        ```pycon
+
+        >>> import torch
+        >>> from karbonn.utils.tracker import TensorTracker
+        >>> tracker = TensorTracker()
+        >>> tracker.update(torch.arange(6))
+        >>> tracker.min()
+        0
+
+        ```
+        """
+        if not self._count:
+            msg = "The tracker is empty"
+            raise EmptyTrackerError(msg)
+        return self._values.values().min().item()
+
+    def quantile(self, q: Tensor, interpolation: str = "linear") -> Tensor:
+        r"""Compute the ``q``-th quantiles.
+
+        Args:
+            q: The ``q``-values in the range ``[0, 1]`` as a
+                ``torch.Tensor`` of type float and shape
+                ``(num_q_values,)``.
+            interpolation: The interpolation method to use when the
+                desired quantile lies between two data points.
+                Can be ``'linear'``, ``'lower'``, ``'higher'``,
+                ``'midpoint'``, and ``'nearest'``.
+
+        Returns:
+            The ``q``-th quantiles as a ``torch.Tensor`` of shape
+                ``(num_q_values,)``
+
+        Raises:
+            EmptyTrackerError: if the tracker is empty.
+
+        Example usage:
+
+        ```pycon
+
+        >>> import torch
+        >>> from karbonn.utils.tracker import TensorTracker
+        >>> tracker = TensorTracker()
+        >>> tracker.update(torch.arange(1001))
+        >>> tracker.quantile(q=torch.tensor([0.1, 0.9]))
+        tensor([100., 900.])
+
+        ```
+        """
+        if not self._count:
+            msg = "The tracker is empty"
+            raise EmptyTrackerError(msg)
+        return quantile(self._values.values().float(), q=q, interpolation=interpolation)
+
+    def std(self) -> float:
+        r"""Get the standard deviation value.
+
+        Returns:
+            The standard deviation value.
+
+        Raises:
+            EmptyTrackerError: if the tracker is empty.
+
+        Example usage:
+
+        ```pycon
+
+        >>> import torch
+        >>> from karbonn.utils.tracker import TensorTracker
+        >>> tracker = TensorTracker()
+        >>> tracker.update(torch.arange(6))
+        >>> tracker.std()
+        1.870...
+
+        ```
+        """
+        if not self._count:
+            msg = "The tracker is empty"
+            raise EmptyTrackerError(msg)
+        return self._values.values().float().std(dim=0).item()
+
+    def sum(self) -> int | float:
+        r"""Get the sum of all the values.
+
+        Returns:
+            The sum of all the values.
+
+        Raises:
+            EmptyTrackerError: if the tracker is empty.
+
+        Example usage:
+
+        ```pycon
+
+        >>> import torch
+        >>> from karbonn.utils.tracker import TensorTracker
+        >>> tracker = TensorTracker()
+        >>> tracker.update(torch.arange(6))
+        >>> tracker.sum()
+        15
+
+        ```
+        """
+        if not self._count:
+            msg = "The tracker is empty"
+            raise EmptyTrackerError(msg)
+        return self._values.values().sum().item()
+
+    def all_reduce(self) -> TensorTracker:
+        r"""Reduce the tracker values across all machines in such a way
+        that all get the final result.
+
+        Returns:
+            The reduced tracker.
+
+        Example usage:
+
+        ```pycon
+
+        >>> import torch
+        >>> from karbonn.utils.tracker import TensorTracker
+        >>> tracker = TensorTracker()
+        >>> tracker.update(torch.arange(6))
+        >>> reduced_tracker = tracker.all_reduce()
+
+        ```
+        """
+        return TensorTracker(self._values.all_reduce().values())
+
+    def clone(self) -> TensorTracker:
+        r"""Create a copy of the current tracker.
+
+        Returns:
+            A copy of the current tracker.
+
+        Example usage:
+
+        ```pycon
+
+        >>> import torch
+        >>> from karbonn.utils.tracker import TensorTracker
+        >>> tracker = TensorTracker(torch.arange(6))
+        >>> tracker_cloned = tracker.clone()
+        >>> tracker.update(torch.ones(3))
+        >>> tracker.sum()
+        18.0
+        >>> tracker_cloned.sum()
+        15
+
+        ```
+        """
+        return TensorTracker(self._values.clone().values())
+
+    def equal(self, other: Any) -> bool:
+        r"""Indicate if two trackers are equal or not.
+
+        Args:
+            other: The value to compare.
+
+        Returns:
+            ``True`` if the trackers are equal,
+                ``False`` otherwise.
+
+        Example usage:
+
+        ```pycon
+
+        >>> import torch
+        >>> from karbonn.utils.tracker import TensorTracker
+        >>> tracker1 = TensorTracker(torch.arange(6))
+        >>> tracker2 = TensorTracker(torch.ones(3))
+        >>> tracker1.equal(tracker2)
+        False
+
+        ```
+        """
+        if not isinstance(other, TensorTracker):
+            return False
+        return objects_are_equal(self.state_dict(), other.state_dict())
+
+    def merge(self, trackers: Iterable[TensorTracker]) -> TensorTracker:
+        r"""Merge several trackers with the current tracker and returns a
+        new tracker.
+
+        Args:
+            trackers: The trackers to merge to the current tracker.
+
+        Returns:
+            The merged tracker.
+
+        Example usage:
+
+        ```pycon
+
+        >>> import torch
+        >>> from karbonn.utils.tracker import TensorTracker
+        >>> tracker1 = TensorTracker(torch.arange(6) + 3)
+        >>> tracker2 = TensorTracker(torch.ones(3))
+        >>> tracker3 = tracker1.merge([tracker2])
+        >>> tracker3.count
+        9
+        >>> tracker3.max()
+        8.0
+        >>> tracker3.min()
+        1.0
+        >>> tracker3.sum()
+        36.0
+
+        ```
+        """
+        values = self._values.clone()
+        for tracker in trackers:
+            values.update(tracker._values.values())
+        return TensorTracker(values.values())
+
+    def merge_(self, trackers: Iterable[TensorTracker]) -> None:
+        r"""Merge several trackers into the current tracker.
+
+        In-place version of ``merge``.
+
+        Args:
+            trackers: The trackers to merge to the current tracker.
+
+        Example usage:
+
+        ```pycon
+
+        >>> import torch
+        >>> from karbonn.utils.tracker import TensorTracker
+        >>> tracker1 = TensorTracker(torch.arange(6) + 3)
+        >>> tracker2 = TensorTracker(torch.ones(3))
+        >>> tracker1.merge_([tracker2])
+        >>> tracker1.count
+        9
+        >>> tracker1.max()
+        8.0
+        >>> tracker1.min()
+        1.0
+        >>> tracker1.sum()
+        36.0
+
+        ```
+        """
+        for tracker in trackers:
+            self.update(tracker._values.values())
+
+    def load_state_dict(self, state_dict: dict[str, Any]) -> None:
+        r"""Load a state to the history tracker.
+
+        Args:
+            state_dict: A dictionary containing state keys with values.
+
+        Example usage:
+
+        ```pycon
+
+        >>> import torch
+        >>> from karbonn.utils.tracker import TensorTracker
+        >>> tracker = TensorTracker()
+        >>> tracker.load_state_dict({"values": torch.arange(6)})
+        >>> tracker.count
+        6
+
+        ```
+        """
+        self.reset()
+        self.update(state_dict["values"])
+
+    def state_dict(self) -> dict[str, Tensor]:
+        r"""Return a dictionary containing state values.
+
+        Returns:
+            The state values in a dict.
+
+        Example usage:
+
+        ```pycon
+
+        >>> import torch
+        >>> from karbonn.utils.tracker import TensorTracker
+        >>> tracker = TensorTracker(torch.arange(6))
+        >>> tracker.state_dict()
+        {'values': tensor([0, 1, 2, 3, 4, 5])}
+
+        ```
+        """
+        return {"values": self._values.values()}
