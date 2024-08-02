5e5a87ea3dc42c67bd448889edb1050b8e24b919
Thibaut Durand <durand.tibo@gmail.com>
2024-08-02 15:52:52 -0700

    Add `MeanErrorState`

--- src/karbonn/metric/state/__init__.py
+++ src/karbonn/metric/state/__init__.py
@@ -2,6 +2,7 @@ r"""Contain the metric states."""
 
 from __future__ import annotations
 
-__all__ = ["BaseState"]
+__all__ = ["BaseState", "MeanErrorState"]
 
 from karbonn.metric.state.base import BaseState
+from karbonn.metric.state.error import MeanErrorState
