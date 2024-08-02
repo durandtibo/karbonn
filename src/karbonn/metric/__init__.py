b30cdd5b64cfddc98dfce171c07d133b84e3d5b5
Thibaut Durand <durand.tibo@gmail.com>
2024-08-02 14:19:26 -0700

    Add `BaseMetric` (#225)

---
+++ src/karbonn/metric/__init__.py
@@ -0,0 +1,7 @@
+r"""Contain the metrics."""
+
+from __future__ import annotations
+
+__all__ = ["BaseMetric", "EmptyMetricError", "setup_metric"]
+
+from karbonn.metric.base import BaseMetric, EmptyMetricError, setup_metric
