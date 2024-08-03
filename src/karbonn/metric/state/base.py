515308ebd9d957acb6d5a09a8567deb720e0987c
Thibaut Durand <durand.tibo@gmail.com>
2024-08-03 08:44:27 -0700

    Add `setup_state` and `is_state_config` (#231)

--- src/karbonn/metric/state/base.py
+++ src/karbonn/metric/state/base.py
@@ -2,16 +2,27 @@ r"""Contain the base class to implement a metric state."""
 
 from __future__ import annotations
 
-__all__ = ["BaseState"]
+__all__ = ["BaseState", "is_state_config", "setup_state"]
 
 import logging
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING, Any
+from unittest.mock import Mock
 
-from objectory import AbstractFactory
+from karbonn.utils.imports import check_objectory, is_objectory_available
 
 if TYPE_CHECKING:
     from minrecord import BaseRecord
+
+
+if is_objectory_available():
+    import objectory
+    from objectory import AbstractFactory
+else:  # pragma: no cover
+    objectory = Mock()
+
+    class AbstractFactory: ...
+
 
 logger = logging.getLogger(__name__)
 
@@ -46,9 +57,6 @@ class BaseState(ABC, metaclass=AbstractFactory):
 
     ```
     """
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__qualname__}(num_predictions={self.num_predictions:,})"
 
     @property
     @abstractmethod
@@ -166,3 +174,65 @@ class BaseState(ABC, metaclass=AbstractFactory):
 
         ```
         """
+
+
+def is_state_config(config: dict) -> bool:
+    r"""Indicate if the input configuration is a configuration for a
+    ``BaseState``.
+
+    This function only checks if the value of the key  ``_target_``
+    is valid. It does not check the other values. If ``_target_``
+    indicates a function, the returned type hint is used to check
+    the class.
+
+    Args:
+        config: The configuration to check.
+
+    Returns:
+        ``True`` if the input configuration is a configuration
+            for a ``BaseState`` object, otherwise ``False``.
+
+    Example usage:
+
+    ```pycon
+
+    >>> from karbonn.metric.state import is_state_config
+    >>> is_state_config({"_target_": "karbonn.metric.state.ErrorState"})
+    True
+
+    ```
+    """
+    check_objectory()
+    return objectory.utils.is_object_config(config, BaseState)
+
+
+def setup_state(state: BaseState | dict) -> BaseState:
+    r"""Set up a ``BaseState`` object.
+
+    Args:
+        state: The state or its configuration.
+
+    Returns:
+        The instantiated ``BaseState`` object.
+
+    Example usage:
+
+    ```pycon
+
+    >>> from karbonn.metric.state import setup_state
+    >>> state = setup_state({"_target_": "karbonn.metric.state.ErrorState"})
+    >>> state
+    ErrorState(
+      (meter): ScalableTensorTracker(count=0, total=0.0, min_value=inf, max_value=-inf)
+      (track_num_predictions): True
+    )
+
+    ```
+    """
+    if isinstance(state, dict):
+        logger.info("Initializing a 'BaseState' object from its configuration...")
+        check_objectory()
+        state = BaseState.factory(**state)
+    if not isinstance(state, BaseState):
+        logger.warning(f"state is not a 'BaseState' (received: {type(state)})")
+    return state
