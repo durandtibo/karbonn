d899b48b36092428ff7dec3e2171a97568a13b77
Thibaut Durand <durand.tibo@gmail.com>
2024-08-03 08:45:54 -0700

    Add `ExtendedErrorState`

--- src/karbonn/metric/state/__init__.py
+++ src/karbonn/metric/state/__init__.py
@@ -2,7 +2,14 @@ r"""Contain the metric states."""
 
 from __future__ import annotations
 
-__all__ = ["BaseState", "ErrorState", "MeanErrorState", "ExtendedErrorState", "is_state_config", "setup_state"]
+__all__ = [
+    "BaseState",
+    "ErrorState",
+    "MeanErrorState",
+    "ExtendedErrorState",
+    "is_state_config",
+    "setup_state",
+]
 
 from karbonn.metric.state.base import BaseState, is_state_config, setup_state
 from karbonn.metric.state.error import ErrorState, ExtendedErrorState, MeanErrorState
