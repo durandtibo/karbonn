1d64d0b1e39556fb507f078e93aee3a8093ee78e
Thibaut Durand <durand.tibo@gmail.com>
2024-08-04 00:47:01 -0700

    Add `AbsoluteError` (#239)

--- src/karbonn/metric/__init__.py
+++ src/karbonn/metric/__init__.py
@@ -2,6 +2,8 @@ r"""Contain the metrics."""
 
 from __future__ import annotations
 
-__all__ = ["BaseMetric", "EmptyMetricError", "setup_metric"]
+__all__ = ["BaseMetric", "EmptyMetricError", "setup_metric", "BaseStateMetric", "AbsoluteError"]
 
 from karbonn.metric.base import BaseMetric, EmptyMetricError, setup_metric
+from karbonn.metric.regression import AbsoluteError
+from karbonn.metric.state_ import BaseStateMetric
---
+++ src/karbonn/metric/regression/__init__.py
@@ -0,0 +1,7 @@
+r"""Contain the regression metrics."""
+
+from __future__ import annotations
+
+__all__ = ["AbsoluteError"]
+
+from karbonn.metric.regression.absolute_error import AbsoluteError
