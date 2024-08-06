d06c7a482c28275bee979f98b2dc4ab843c3233c
Thibaut Durand <durand.tibo@gmail.com>
2024-08-05 22:15:17 -0700

    Add `CategoricalAccuracy`

--- src/karbonn/metric/classification/accuracy.py
+++ src/karbonn/metric/classification/accuracy.py
@@ -2,7 +2,7 @@ r"""Contain accuracy metrics."""
 
 from __future__ import annotations
 
-__all__ = ["BinaryAccuracy"]
+__all__ = ["BinaryAccuracy", "CategoricalAccuracy"]
 
 import logging
 from typing import TYPE_CHECKING
@@ -81,7 +81,7 @@ class BinaryAccuracy(BaseStateMetric):
                 should be set to transform the predictions/scores
                 to binary labels where the values are ``0`` or ``1``.
             target: The binary targets where the values are  ``0`` or
-                ``1``. This input must be a ``torch.Tensor``  of shape
+                ``1``. This input must be a ``torch.Tensor`` of shape
                 ``(d0, d1, ..., dn)`` or  ``(d0, d1, ..., dn, 1)``
                 and type bool or long or float.
 
@@ -95,6 +95,90 @@ class BinaryAccuracy(BaseStateMetric):
         >>> metric(torch.zeros(4), torch.ones(4))
         >>> metric.value()
         {'accuracy': 0.0, 'num_predictions': 4}
+
+        ```
+        """
+        prediction = self.transform(prediction)
+        self._state.update(prediction.eq(target.view_as(prediction)))
+
+
+class CategoricalAccuracy(BaseStateMetric):
+    r"""Implement a categorical accuracy metric.
+
+    Args:
+    ----
+        mode (str): Specifies the mode.
+        name (str, optional): Specifies the name used to log the
+            metric. Default: ``'cat_acc'``
+        state (``BaseState`` or dict or ``None``, optional): Specifies
+            the metric state or its configuration. If ``None``,
+            ``AccuracyState`` is instantiated. Default: ``None``
+
+    Example usage:
+
+    ```pycon
+
+    >>> import torch
+    >>> from karbonn.metric import CategoricalAccuracy
+    >>> metric = CategoricalAccuracy()
+    >>> metric
+    CategoricalAccuracy(
+      (state): AccuracyState(
+          (tracker): MeanTensorTracker(count=0, total=0.0)
+          (track_num_predictions): True
+        )
+      (transform): Identity()
+    )
+    >>> metric(torch.tensor([1, 0]), torch.tensor([1, 0]))
+    >>> metric.value()
+    {'accuracy': 1.0, 'num_predictions': 2}
+    >>> metric(torch.tensor([[0, 2]]), torch.tensor([1, 2]))
+    >>> metric.value()
+    {'accuracy': 0.75, 'num_predictions': 4}
+    >>> metric.reset()
+    >>> metric(torch.tensor([[1, 1]]), torch.tensor([1, 2]))
+    >>> metric.value()
+    {'accuracy': 0.5, 'num_predictions': 2}
+
+    ```
+    """
+
+    def __init__(
+        self,
+        state: BaseState | dict | None = None,
+        transform: Module | dict | None = None,
+    ) -> None:
+        super().__init__(state=state or AccuracyState())
+        self.transform = setup_module(transform or Identity())
+
+    def forward(self, prediction: Tensor, target: Tensor) -> None:
+        r"""Update the accuracy metric given a mini-batch of examples.
+
+        Args:
+            prediction: The predicted labels or the predictions.
+                This input must be a ``torch.Tensor`` of  shape
+                ``(d0, d1, ..., dn)`` or
+                ``(d0, d1, ..., dn, num_classes)``
+                and type long or float. If the input is the
+                predictions/scores, then the ``transform`` module
+                should be set to transform the predictions/scores
+                to categorical labels where the values are in
+                ``{0, 1, ..., num_classes-1}``.
+            target: The categorical targets. The values have to be in
+                ``{0, 1, ..., num_classes-1}``. This input must be a
+                ``torch.Tensor`` of shape ``(d0, d1, ..., dn)`` or
+                ``(d0, d1, ..., dn, 1)`` and type long or float.
+
+        Example usage:
+
+        ```pycon
+
+        >>> import torch
+        >>> from karbonn.metric import CategoricalAccuracy
+        >>> metric = CategoricalAccuracy()
+        >>> metric(torch.tensor([1, 2, 0, 1]), torch.tensor([1, 2, 0, 1]))
+        >>> metric.value()
+        {'accuracy': 1.0, 'num_predictions': 4}
 
         ```
         """
