804f086e593e70e7732b22ff127753a696cdc90e
Thibaut Durand <durand.tibo@gmail.com>
2024-08-08 21:20:44 -0700

    Fix bug in tests when `objectory` is not available

--- tests/unit/metric/test_base.py
+++ tests/unit/metric/test_base.py
@@ -2,14 +2,19 @@ from __future__ import annotations
 
 import logging
 from collections import defaultdict
-from typing import TYPE_CHECKING
+from unittest.mock import patch
 
-from objectory import OBJECT_TARGET
+import pytest
 
 from karbonn.metric import AbsoluteError, setup_metric
+from karbonn.testing import objectory_available
+from karbonn.utils.imports import is_objectory_available
 
-if TYPE_CHECKING:
-    import pytest
+if is_objectory_available():
+    from objectory import OBJECT_TARGET
+else:  # pragma: no cover
+    OBJECT_TARGET = "_target_"
+
 
 ##################################
 #     Tests for setup_metric     #
@@ -21,6 +26,7 @@ def test_setup_metric_object() -> None:
     assert setup_metric(metric) is metric
 
 
+@objectory_available
 def test_setup_metric_config() -> None:
     assert isinstance(
         setup_metric({OBJECT_TARGET: "karbonn.metric.AbsoluteError"}),
@@ -28,7 +34,16 @@ def test_setup_metric_config() -> None:
     )
 
 
+@objectory_available
 def test_setup_metric_incorrect_type(caplog: pytest.LogCaptureFixture) -> None:
     with caplog.at_level(level=logging.WARNING):
         assert isinstance(setup_metric({OBJECT_TARGET: "collections.defaultdict"}), defaultdict)
         assert caplog.messages
+
+
+def test_setup_metric_object_no_objectory() -> None:
+    with (
+        patch("karbonn.utils.imports.is_objectory_available", lambda: False),
+        pytest.raises(RuntimeError, match="'objectory' package is required but not installed."),
+    ):
+        setup_metric({OBJECT_TARGET: "karbonn.metric.AbsoluteError"})
