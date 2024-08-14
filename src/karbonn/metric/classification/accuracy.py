7e74b912463264f5f075f758fa15544d3f20be92
Thibaut Durand <durand.tibo@gmail.com>
2024-08-13 22:15:38 -0700

    Remove `BinaryAccuracy` (#296)

--- src/karbonn/metric/classification/accuracy.py
+++ src/karbonn/metric/classification/accuracy.py
@@ -2,7 +2,7 @@ r"""Contain accuracy metrics."""
 
 from __future__ import annotations
 
-__all__ = ["BinaryAccuracy", "CategoricalAccuracy", "TopKAccuracy"]
+__all__ = ["CategoricalAccuracy", "TopKAccuracy"]
 
 import logging
 from typing import TYPE_CHECKING
@@ -22,89 +22,6 @@ if TYPE_CHECKING:
     from torch import Tensor
 
 logger = logging.getLogger(__name__)
-
-
-class BinaryAccuracy(BaseStateMetric):
-    r"""Implement the binary accuracy metric.
-
-    Args:
-        state: The metric state or its configuration. If ``None``,
-            ``AccuracyState`` is instantiated.
-        transform: The transformation applied on the predictions to
-            generate the predicted binary labels. If ``None``, the
-            identity module is used. The transform module must take
-            a single input tensor and output a single tensor with the
-            same shape as the input.
-
-    Example usage:
-
-    ```pycon
-
-    >>> import torch
-    >>> from karbonn.metric import BinaryAccuracy
-    >>> metric = BinaryAccuracy()
-    >>> metric
-    BinaryAccuracy(
-      (state): AccuracyState(
-          (tracker): MeanTensorTracker(count=0, total=0.0)
-          (track_count): True
-        )
-      (transform): Identity()
-    )
-    >>> metric(torch.zeros(4), torch.ones(4))
-    >>> metric.value()
-    {'accuracy': 0.0, 'count': 4}
-    >>> metric(torch.ones(4), torch.ones(4))
-    >>> metric.value()
-    {'accuracy': 0.5, 'count': 8}
-    >>> metric.reset()
-    >>> metric(torch.ones(4), torch.ones(4))
-    >>> metric.value("bin_acc_")
-    {'bin_acc_accuracy': 1.0, 'bin_acc_count': 4}
-
-    ```
-    """
-
-    def __init__(
-        self,
-        state: BaseState | dict | None = None,
-        transform: Module | dict | None = None,
-    ) -> None:
-        super().__init__(state=state or AccuracyState())
-        self.transform = setup_module(transform or Identity())
-
-    def forward(self, prediction: Tensor, target: Tensor) -> None:
-        r"""Update the binary accuracy metric given a mini-batch of
-        examples.
-
-        Args:
-            prediction: The predictions or the predicted labels.
-                This input must be a ``torch.Tensor`` of  shape
-                ``(d0, d1, ..., dn)`` or ``(d0, d1, ..., dn, 1)``
-                and type bool or long or float. If the input is the
-                predictions/scores, then the ``transform`` module
-                should be set to transform the predictions/scores
-                to binary labels where the values are ``0`` or ``1``.
-            target: The binary targets where the values are  ``0`` or
-                ``1``. This input must be a ``torch.Tensor`` of shape
-                ``(d0, d1, ..., dn)`` or  ``(d0, d1, ..., dn, 1)``
-                and type bool or long or float.
-
-        Example usage:
-
-        ```pycon
-
-        >>> import torch
-        >>> from karbonn.metric import BinaryAccuracy
-        >>> metric = BinaryAccuracy()
-        >>> metric(torch.zeros(4), torch.ones(4))
-        >>> metric.value()
-        {'accuracy': 0.0, 'count': 4}
-
-        ```
-        """
-        prediction = self.transform(prediction)
-        self._state.update(prediction.eq(target.view_as(prediction)))
 
 
 class CategoricalAccuracy(BaseStateMetric):
