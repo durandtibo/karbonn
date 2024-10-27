1e0226ddf5bedd503144e56bbeb77be043b54722
Thibaut Durand <durand.tibo@gmail.com>
2024-10-27 12:20:07 -0700

    Add `poisson_regression_loss` (#369)

--- src/karbonn/functional/loss/__init__.py
+++ src/karbonn/functional/loss/__init__.py
@@ -17,11 +17,13 @@ __all__ = [
     "relative_loss",
     "reversed_relative_indicator",
     "quantile_regression_loss",
+    "poisson_regression_loss",
 ]
 
 from karbonn.functional.loss.asinh import asinh_mse_loss, asinh_smooth_l1_loss
 from karbonn.functional.loss.general_robust import general_robust_regression_loss
 from karbonn.functional.loss.log import log_cosh_loss, msle_loss
+from karbonn.functional.loss.poisson import poisson_regression_loss
 from karbonn.functional.loss.quantile import quantile_regression_loss
 from karbonn.functional.loss.relative import (
     arithmetical_mean_indicator,
