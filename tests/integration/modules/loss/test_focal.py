e76b2d8fe960a0de61a96cb5fc3aaf4ba64b5ddc
Thibaut Durand <durand.tibo@gmail.com>
2024-11-01 00:13:15 -0700

    Add new implementation for `BinaryFocalLoss`

--- tests/integration/modules/loss/test_focal.py
+++ tests/integration/modules/loss/test_focal.py
@@ -9,7 +9,7 @@ from karbonn.utils import is_loss_decreasing_with_adam
 
 def test_binary_focal_loss() -> None:
     assert is_loss_decreasing_with_adam(
-        module=nn.Sequential(nn.Linear(4, 4), nn.ReLU(), nn.Linear(8, 4), nn.Sigmoid()),
+        module=nn.Sequential(nn.Linear(4, 8), nn.ReLU(), nn.Linear(8, 4), nn.Sigmoid()),
         criterion=BinaryFocalLoss(),
         feature=torch.randn(8, 4),
         target=torch.randint(0, 2, size=(8, 4), dtype=torch.float),
