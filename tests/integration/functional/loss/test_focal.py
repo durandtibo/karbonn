aa0155bf60670b5ddf45249407d4f944f0bbe212
Thibaut Durand <durand.tibo@gmail.com>
2024-11-01 00:37:34 -0700

    Add more integration tests

---
+++ tests/integration/functional/loss/test_focal.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import torch
+from torch import nn
+
+from karbonn.functional import binary_focal_loss, binary_focal_loss_with_logits
+from karbonn.utils import is_loss_decreasing_with_adam
+
+
+def test_binary_focal_loss() -> None:
+    assert is_loss_decreasing_with_adam(
+        module=nn.Sequential(nn.Linear(4, 8), nn.ReLU(), nn.Linear(8, 4), nn.Sigmoid()),
+        criterion=binary_focal_loss,
+        feature=torch.randn(8, 4),
+        target=torch.randint(0, 2, size=(8, 4), dtype=torch.float),
+    )
+
+
+def test_binary_focal_loss_with_logits() -> None:
+    assert is_loss_decreasing_with_adam(
+        module=nn.Sequential(nn.Linear(4, 8), nn.ReLU(), nn.Linear(8, 4)),
+        criterion=binary_focal_loss_with_logits,
+        feature=torch.randn(8, 4),
+        target=torch.randint(0, 2, size=(8, 4), dtype=torch.float),
+    )
