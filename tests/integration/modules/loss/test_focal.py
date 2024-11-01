a611fba91079f7241f7b5433c53b541087f94b1a
Thibaut Durand <durand.tibo@gmail.com>
2024-10-31 23:42:36 -0700

    Remove `BinaryFocalLoss`

--- tests/integration/modules/loss/test_focal.py
+++ tests/integration/modules/loss/test_focal.py
@@ -1,26 +1,20 @@
 from __future__ import annotations
 
-import pytest
 import torch
 from torch import nn
 
-from karbonn.modules import binary_focal_loss
+from karbonn.modules import BinaryFocalLossWithLogits
 from karbonn.utils import is_loss_decreasing_with_adam
 
-SIZES = (1, 2)
-TOLERANCE = 1e-6
+##############################################
+#     Tests of BinaryFocalLossWithLogits     #
+##############################################
 
 
-####################################
-#     Tests of BinaryFocalLoss     #
-####################################
-
-
-@pytest.mark.parametrize("logits", {True, False})
-def test_binary_focal_loss(logits: bool) -> None:
+def test_binary_focal_loss_with_logits() -> None:
     assert is_loss_decreasing_with_adam(
         module=nn.Sequential(nn.Linear(4, 4), nn.Sigmoid()),
-        criterion=binary_focal_loss(logits=logits),
+        criterion=BinaryFocalLossWithLogits(),
         feature=torch.rand(8, 4),
         target=torch.randint(0, 2, size=(8, 4), dtype=torch.float),
     )
