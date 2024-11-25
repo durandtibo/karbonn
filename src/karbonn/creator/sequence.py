e3edd808990bb2f75e507a94b36b44f69b1407f5
Thibaut Durand <durand.tibo@gmail.com>
2024-11-25 12:46:08 -0800

    Add `ListCreator` (#423)

--- src/karbonn/creator/sequence.py
+++ src/karbonn/creator/sequence.py
@@ -2,13 +2,13 @@ r"""Contain object creator implementations."""
 
 from __future__ import annotations
 
-__all__ = ["CreatorList"]
+__all__ = ["CreatorList", "CreatorTuple", "ListCreator"]
 
 from typing import TYPE_CHECKING, TypeVar
 
 from coola.utils import repr_indent, repr_sequence, str_indent, str_sequence
 
-from karbonn.creator.base import BaseCreator
+from karbonn.creator.base import BaseCreator, setup_creator
 from karbonn.utils.factory import setup_object
 
 if TYPE_CHECKING:
@@ -105,3 +105,57 @@ class CreatorTuple(BaseCreator[T]):
 
     def create(self) -> tuple[T, ...]:
         return tuple(setup_object(item) for item in self._items)
+
+
+class ListCreator(BaseCreator[T]):
+    r"""Implement a list object creator.
+
+    Args:
+        creators: The sequence of object creators or their
+            configurations.
+
+    Example usage:
+
+    ```pycon
+
+    >>> from karbonn.creator import ListCreator, Creator
+    >>> creator = ListCreator(
+    ...     [
+    ...         Creator(
+    ...             {
+    ...                 "_target_": "torch.nn.Linear",
+    ...                 "in_features": 4,
+    ...                 "out_features": 6,
+    ...             }
+    ...         ),
+    ...         Creator({"_target_": "torch.nn.Identity"}),
+    ...     ]
+    ... )
+    >>> creator
+    ListCreator(
+      (0): Creator(
+          (_target_): torch.nn.Linear
+          (in_features): 4
+          (out_features): 6
+        )
+      (1): Creator(
+          (_target_): torch.nn.Identity
+        )
+    )
+    >>> creator.create()
+    [Linear(in_features=4, out_features=6, bias=True), Identity()]
+
+    ```
+    """
+
+    def __init__(self, creators: Sequence[BaseCreator[T] | dict]) -> None:
+        self._creators = tuple(setup_creator(creator) for creator in creators)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__qualname__}(\n  {repr_indent(repr_sequence(self._creators))}\n)"
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__qualname__}(\n  {str_indent(str_sequence(self._creators))}\n)"
+
+    def create(self) -> list[T]:
+        return [creator.create() for creator in self._creators]
