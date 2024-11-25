49ad6da6e58bf55c0e9b9b96b34a83577c410a65
Thibaut Durand <durand.tibo@gmail.com>
2024-11-25 12:23:48 -0800

    Add `CreatorTuple` (#422)

--- src/karbonn/creator/sequence.py
+++ src/karbonn/creator/sequence.py
@@ -60,3 +60,48 @@ class CreatorList(BaseCreator[T]):
 
     def create(self) -> list[T]:
         return [setup_object(item) for item in self._items]
+
+
+class CreatorTuple(BaseCreator[T]):
+    r"""Implement a tuple object creator.
+
+    Args:
+        items: The sequence of objects or their configurations.
+
+    Example usage:
+
+    ```pycon
+
+    >>> from karbonn.creator import CreatorTuple
+    >>> creator = CreatorTuple(
+    ...     [
+    ...         {
+    ...             "_target_": "torch.nn.Linear",
+    ...             "in_features": 4,
+    ...             "out_features": 6,
+    ...         },
+    ...         {"_target_": "torch.nn.Identity"},
+    ...     ]
+    ... )
+    >>> creator
+    CreatorTuple(
+      (0): {'_target_': 'torch.nn.Linear', 'in_features': 4, 'out_features': 6}
+      (1): {'_target_': 'torch.nn.Identity'}
+    )
+    >>> creator.create()
+    (Linear(in_features=4, out_features=6, bias=True), Identity())
+
+    ```
+    """
+
+    def __init__(self, items: Sequence[T | dict]) -> None:
+        self._items = items
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__qualname__}(\n  {repr_indent(repr_sequence(self._items))}\n)"
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__qualname__}(\n  {str_indent(str_sequence(self._items))}\n)"
+
+    def create(self) -> tuple[T, ...]:
+        return tuple(setup_object(item) for item in self._items)
