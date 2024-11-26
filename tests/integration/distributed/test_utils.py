f093ef25e6c219b71def7f1ec203b6f7693de978
Thibaut Durand <durand.tibo@gmail.com>
2024-11-26 11:00:26 -0800

    Add integration tests

--- tests/integration/distributed/test_utils.py
+++ tests/integration/distributed/test_utils.py
@@ -1,6 +1,26 @@
 from __future__ import annotations
 
-from karbonn.distributed import is_distributed, is_main_process
+from torch.distributed import Backend
+
+from karbonn.distributed import (
+    distributed_context,
+    gloocontext,
+    is_distributed,
+    is_main_process,
+    ncclcontext,
+)
+from karbonn.testing import (
+    cuda_available,
+    distributed_available,
+    gloo_available,
+    ignite_available,
+    nccl_available,
+)
+from karbonn.utils.imports import is_ignite_available
+
+if is_ignite_available():  # pragma: no cover
+    from ignite import distributed as idist
+
 
 ####################################
 #     Tests for is_distributed     #
@@ -18,3 +38,56 @@ def test_is_distributed() -> None:
 
 def test_is_main_process() -> None:
     assert is_main_process()
+
+
+########################################
+#    Tests for distributed_context     #
+########################################
+
+
+@ignite_available
+@distributed_available
+@gloo_available
+def test_distributed_context_gloo() -> None:
+    with distributed_context(Backend.GLOO):
+        assert idist.backend() == Backend.GLOO
+    assert idist.backend() is None
+
+
+@ignite_available
+@distributed_available
+@cuda_available
+@nccl_available
+def test_distributed_context_nccl() -> None:
+    with distributed_context(Backend.NCCL):
+        assert idist.backend() == Backend.NCCL
+    assert idist.backend() is None
+
+
+#################################
+#     Tests for gloocontext     #
+#################################
+
+
+@ignite_available
+@distributed_available
+@gloo_available
+def test_gloocontext() -> None:
+    with gloocontext():
+        assert idist.backend() == Backend.GLOO
+    assert idist.backend() is None
+
+
+#################################
+#     Tests for ncclcontext     #
+#################################
+
+
+@ignite_available
+@distributed_available
+@cuda_available
+@nccl_available
+def test_ncclcontext() -> None:
+    with ncclcontext():
+        assert idist.backend() == Backend.NCCL
+    assert idist.backend() is None
