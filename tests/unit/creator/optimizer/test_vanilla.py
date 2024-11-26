568478e912f7945490c3e6100f06690ab180f752
Thibaut Durand <durand.tibo@gmail.com>
2024-11-26 08:08:50 -0800

    Improve some tests (#414)

--- tests/unit/creator/optimizer/test_vanilla.py
+++ tests/unit/creator/optimizer/test_vanilla.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
+import operator
+
 import pytest
 import torch
+from feu import compare_version
 from torch import nn
 from torch.optim import SGD, Optimizer
 
@@ -13,6 +16,25 @@ if is_objectory_available():
     from objectory import OBJECT_TARGET
 else:  # pragma: no cover
     OBJECT_TARGET = "_target_"
+
+OPTIMIZERS = [
+    ({OBJECT_TARGET: "torch.optim.Adadelta"}, torch.optim.Adadelta),
+    ({OBJECT_TARGET: "torch.optim.Adagrad"}, torch.optim.Adagrad),
+    ({OBJECT_TARGET: "torch.optim.Adam"}, torch.optim.Adam),
+    ({OBJECT_TARGET: "torch.optim.AdamW"}, torch.optim.AdamW),
+    ({OBJECT_TARGET: "torch.optim.SparseAdam"}, torch.optim.SparseAdam),
+    ({OBJECT_TARGET: "torch.optim.Adamax"}, torch.optim.Adamax),
+    ({OBJECT_TARGET: "torch.optim.ASGD"}, torch.optim.ASGD),
+    ({OBJECT_TARGET: "torch.optim.LBFGS"}, torch.optim.LBFGS),
+    ({OBJECT_TARGET: "torch.optim.NAdam"}, torch.optim.NAdam),
+    ({OBJECT_TARGET: "torch.optim.RAdam"}, torch.optim.RAdam),
+    ({OBJECT_TARGET: "torch.optim.RMSprop"}, torch.optim.RMSprop),
+    ({OBJECT_TARGET: "torch.optim.Rprop"}, torch.optim.Rprop),
+    ({OBJECT_TARGET: "torch.optim.SGD"}, torch.optim.SGD),
+]
+if compare_version(package="torch", op=operator.ge, version="2.5.0"):
+    OPTIMIZERS.append(({OBJECT_TARGET: "torch.optim.Adafactor"}, torch.optim.Adafactor))
+
 
 ######################################
 #     Tests for OptimizerCreator     #
@@ -40,25 +62,7 @@ def test_optimizer_creator_create() -> None:
     )
 
 
-@pytest.mark.parametrize(
-    ("config", "cls"),
-    [
-        ({OBJECT_TARGET: "torch.optim.Adadelta"}, torch.optim.Adadelta),
-        ({OBJECT_TARGET: "torch.optim.Adafactor"}, torch.optim.Adafactor),
-        ({OBJECT_TARGET: "torch.optim.Adagrad"}, torch.optim.Adagrad),
-        ({OBJECT_TARGET: "torch.optim.Adam"}, torch.optim.Adam),
-        ({OBJECT_TARGET: "torch.optim.AdamW"}, torch.optim.AdamW),
-        ({OBJECT_TARGET: "torch.optim.SparseAdam"}, torch.optim.SparseAdam),
-        ({OBJECT_TARGET: "torch.optim.Adamax"}, torch.optim.Adamax),
-        ({OBJECT_TARGET: "torch.optim.ASGD"}, torch.optim.ASGD),
-        ({OBJECT_TARGET: "torch.optim.LBFGS"}, torch.optim.LBFGS),
-        ({OBJECT_TARGET: "torch.optim.NAdam"}, torch.optim.NAdam),
-        ({OBJECT_TARGET: "torch.optim.RAdam"}, torch.optim.RAdam),
-        ({OBJECT_TARGET: "torch.optim.RMSprop"}, torch.optim.RMSprop),
-        ({OBJECT_TARGET: "torch.optim.Rprop"}, torch.optim.Rprop),
-        ({OBJECT_TARGET: "torch.optim.SGD"}, torch.optim.SGD),
-    ],
-)
+@pytest.mark.parametrize(("config", "cls"), OPTIMIZERS)
 @objectory_available
 def test_optimizer_creator_create_compatibility(config: dict, cls: type[Optimizer]) -> None:
     linear = nn.Linear(in_features=4, out_features=6)
