cfa33292e613a4003a5a018c7e156f024bf06839
Thibaut Durand <durand.tibo@gmail.com>
2024-11-26 10:18:54 -0800

    Add `distributed_context` (#435)

--- src/karbonn/distributed/__init__.py
+++ src/karbonn/distributed/__init__.py
@@ -2,6 +2,11 @@ r"""Contain functions for distributed computing."""
 
 from __future__ import annotations
 
-__all__ = ["is_distributed", "is_main_process"]
+__all__ = ["UnknownBackendError", "distributed_context", "is_distributed", "is_main_process"]
 
-from karbonn.distributed.utils import is_distributed, is_main_process
+from karbonn.distributed.utils import (
+    UnknownBackendError,
+    distributed_context,
+    is_distributed,
+    is_main_process,
+)
