6b6207b76c2acad5ceaaf9ee447d2ed28c96627d
Thibaut Durand <durand.tibo@gmail.com>
2024-11-27 00:27:46 -0800

    Improve some tests

--- tests/integration/functional/loss/test_poisson.py
+++ tests/integration/functional/loss/test_poisson.py
@@ -9,7 +9,7 @@ from karbonn.utils import is_loss_decreasing_with_adam
 
 def test_poisson_regression_loss() -> None:
     assert is_loss_decreasing_with_adam(
-        module=nn.Sequential(nn.Linear(4, 8), nn.ReLU(), nn.Linear(8, 4)),
+        module=nn.Sequential(nn.Linear(4, 8), nn.ReLU(), nn.Linear(8, 4), nn.Softplus()),
         criterion=poisson_regression_loss,
         feature=torch.randn(8, 4),
         target=torch.randn(8, 4),
